3ffc03ca76497c20f348b90c9ce02d0929787cfb
SUPRIYA BARAI <70096639+blackcodding@users.noreply.github.com>
2021-01-03 22:51:02 +0530

    Practice Q-2 03/01/2021

---
+++ Sum of Digits.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution
+{
+public:
+    int sumOfDigits(int N)
+    {
+        int sum = 0;
+        while (N)
+        {
+            sum = sum + (N % 10);
+            N = N / 10;
+        }
+        return sum;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin >> N;
+        Solution ob;
+        cout << ob.sumOfDigits(N) << endl;
+    }
+    return 0;
+} // } Driver Code Ends
