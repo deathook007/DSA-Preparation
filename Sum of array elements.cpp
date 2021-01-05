36a6b0ce62a826795cf1d42129dc6b2f8ac6cea9
SUPRIYA BARAI <70096639+blackcodding@users.noreply.github.com>
2021-01-05 23:41:40 +0530

    Practice Q-5 05/01/2021

---
+++ Sum of array elements.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t, n, i;
+    cin >> t;
+    while (t--)
+    {
+        cin >> n;
+        int a[n];
+        for (i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        int sum = 0;
+        for (i = 0; i < n; i++)
+        {
+            sum = sum + a[i];
+        }
+        cout << sum << endl;
+    }
+    return 0;
+}
