bc10833057cf533b680f6d9de0d663b1aec1e6cb
SUPRIYA BARAI <70096639+blackcodding@users.noreply.github.com>
2021-01-13 22:34:03 +0530

    Practice Q-19 13/01/2021

---
+++ Finding Squreroot.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    int t, n, root;
+    cin >> t;
+    int squre_root(int);
+    while (t--)
+    {
+        cin >> n;
+        root = squre_root(n);
+        cout << root << endl;
+    }
+    return 0;
+}
+int squre_root(int a)
+{
+    int x;
+    x = sqrt(a);
+    return (x);
+}
