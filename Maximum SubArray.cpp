842ef57f48b93c54dc8e1b30db8b3336cca296f1
SUPRIYA BARAI <70096639+blackcodding@users.noreply.github.com>
2021-08-27 15:15:11 +0530

    Interview Question solution on gfg

---
+++ Maximum SubArray.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    int maxSubarraySum(int arr[], int n){
+        int cs = 0;
+        int ms = INT_MIN;
+        for(int i=0; i<n; i++){
+            cs = cs + arr[i];
+            ms = cs > ms ? cs : ms;
+            if(cs<0){
+               cs = 0;
+            }
+        }
+        return ms;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t,n;
+
+    cin>>t; //input testcases
+    while(t--) //while testcases exist
+    {
+
+        cin>>n; //input size of array
+
+        int a[n];
+
+        for(int i=0;i<n;i++)
+            cin>>a[i]; //inputting elements of array
+
+        Solution ob;
+
+        cout << ob.maxSubarraySum(a, n) << endl;
+    }
+}
+  // } Driver Code Ends
