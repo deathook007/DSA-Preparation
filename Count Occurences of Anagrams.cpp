cdd4abfb27cbe610b3d4a3a5528543b2c0962c84
SUPRIYA BARAI <70096639+blackcodding@users.noreply.github.com>
2022-02-03 00:05:21 +0530

    Counting Occurences of Anagrams - GFG

--- Count Occurences of Anagrams.cpp
+++ Count Occurences of Anagrams.cpp
@@ -1,44 +1,39 @@
-#include <bits/stdc++.h>
-
+/* 
+Count Occurences of Anagrams
+Approach - Using Sliding Window and Hashing
+Time Complexity : O(N * 26) = O(N)
+Space Complexity : O(26) = O(1)
+*/
+#include<bits/stdc++.h>
 using namespace std;
-
-class Solution{
-public:
-	int search(string p, string s) {
-	    vector<int> p_hash(26, 0), s_hash(26, 0);
-        int s_len = s.size(), p_len = p.size(), count=0;
-        if(s_len < p_len){
-            return 0;
-        }
-        for(int i=0; i<p_len; i++){
-            p_hash[p[i] - 'a']++;
-            s_hash[s[i] - 'a']++;
-        }
-        if(p_hash == s_hash){
+int countAnagrams(string s, string p){
+    vector<int> sHash(26, 0), pHash(26, 0);
+	int count = 0;
+    int s_len = s.size();
+    int p_len = p.size();
+        
+    for(int i=0; i<p_len; i++){
+        pHash[p[i] - 'a']++;
+        sHash[s[i] - 'a']++;
+    }
+        
+    if(pHash == sHash){
+        count++;
+    }
+        
+    for(int i=p_len; i<s_len; i++){
+        sHash[s[i - p_len] - 'a']--;
+        sHash[s[i] - 'a']++;
+        if(sHash == pHash){
             count++;
         }
-        for(int i=p_len; i<s_len; i++){
-            s_hash[s[i - p_len] - 'a']--;
-            s_hash[s[i] - 'a']++;
-            if(s_hash == p_hash){
-                count++;
-            }
-        }
-        return count;
-	}
-
-};
-
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        string pat, txt;
-        cin >> txt >> pat;
-        Solution ob;
-        auto ans = ob.search(pat, txt);
-        cout << ans << "\n";
     }
+    return count;
+}
+
+int main(){
+    string s = "cbaebabacd";
+    string p = "abc";
+    cout<<countAnagrams(s, p);
     return 0;
 }
